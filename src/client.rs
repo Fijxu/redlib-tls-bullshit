79b995750f1b4d7b3dffbc910f960dcd45160abc
Fijxu <fijxu@nadeko.net>
2026-01-28 01:03:29 -0300

    Set correct set of cipher suites and add http/1.1 ALPN

--- src/client.rs
+++ src/client.rs
@@ -34,20 +34,28 @@ pub static HTTPS_CONNECTOR: LazyLock<HttpsConnector<HttpConnector>> = LazyLock::
 	hyper_rustls::HttpsConnectorBuilder::new()
 		.with_tls_config(
 			rustls::ClientConfig::builder()
-				// These are the Firefox 145.0 cipher suite,
-				// minus the suites missing forward-secrecy support,
-				// in the same order.
-				// https://github.com/redlib-org/redlib/issues/446#issuecomment-3609306592
+				// These are the cipher suites that the Reddit Android app uses.
 				.with_cipher_suites(&[
-					rustls::cipher_suite::TLS13_AES_256_GCM_SHA384,
 					rustls::cipher_suite::TLS13_AES_128_GCM_SHA256,
+					rustls::cipher_suite::TLS13_AES_256_GCM_SHA384,
 					rustls::cipher_suite::TLS13_CHACHA20_POLY1305_SHA256,
 					rustls::cipher_suite::TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
 					rustls::cipher_suite::TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-					rustls::cipher_suite::TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
-					rustls::cipher_suite::TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
 					rustls::cipher_suite::TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
 					rustls::cipher_suite::TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+					rustls::cipher_suite::TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
+					rustls::cipher_suite::TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
+					// Unsupported ciphers that Rustls doesn't allow us to set
+					// because they are weak/insecure, but they are needed since
+					// the Reddit android app uses it.
+					// Reference: https://github.com/rustls/rustls/issues/2414
+					//
+					// rustls::cipher_suite::TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+					// rustls::cipher_suite::TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+					// rustls::cipher_suite::TLS_RSA_WITH_AES_128_GCM_SHA256,
+					// rustls::cipher_suite::TLS_RSA_WITH_AES_256_GCM_SHA384,
+					// rustls::cipher_suite::TLS_RSA_WITH_AES_128_CBC_SHA,
+					// rustls::cipher_suite::TLS_RSA_WITH_AES_256_CBC_SHA,
 				])
 				// .with_safe_default_cipher_suites()
 				.with_safe_default_kx_groups()
@@ -57,6 +65,8 @@ pub static HTTPS_CONNECTOR: LazyLock<HttpsConnector<HttpConnector>> = LazyLock::
 				.with_no_client_auth(),
 		)
 		.https_only()
+		// The Reddit android app sends ALPN `h2,http/1.1`
+		.enable_http1()
 		.enable_http2()
 		.build()
 });
